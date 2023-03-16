8663deafd60b77e9f402122159eda3223d4a829a
casjay <git-admin@casjaysdev.com>
2023-03-16 01:49:23 -0400

    🗃️ Committing everything that changed 🗃️

--- README.md
+++ README.md
@@ -1,10 +1,36 @@
-<h1 align=center>
-👋 Welcome to caddy 👋
-</h1>
-<p align=center>
-StartDocumentationHere
-</p>
-  
-## Author  
+## 👋 Welcome to caddy 🚀  
 
-👤 **Jason Hempstead**  
+caddy README  
+  
+  
+## Install my system scripts  
+
+```shell
+ sudo bash -c "$(curl -q -LSsf "https://github.com/systemmgr/installer/raw/main/install.sh")"
+ sudo systemmgr --config && sudo systemmgr install scripts  
+```
+
+## Get source files  
+
+```shell
+dockermgr download src caddy
+```
+
+OR
+
+```shell
+git clone "https://github.com/casjaysdevdocker/caddy" "$HOME/Projects/github/casjaysdevdocker/caddy"
+```
+
+## Build container  
+
+```shell
+cd "$HOME/Projects/github/casjaysdevdocker/caddy"
+buildx 
+```
+
+## Authors  
+
+📽 dockermgr: [Github](https://github.com/dockermgr) 📽  
+🤖 casjay: [Github](https://github.com/casjay) [Docker](https://hub.docker.com/r/casjay) 🤖  
+⛵ CasjaysDevDocker: [Github](https://github.com/casjaysdevdocker) [Docker](https://hub.docker.com/r/casjaysdevdocker) ⛵  
--- rootfs/usr/local/share/template-files/data/htdocs/www/index.php
+++ rootfs/usr/local/share/template-files/data/htdocs/www/index.php
@@ -10,8 +10,8 @@
 # @@ReadME           :  
 # @@Copyright        :  Copyright: (c) 2023 Jason Hempstead, Casjays Developments
 # @@Created          :  Thursday, Mar 09, 2023 18:46 EST
-# @@File             :  info.php
-# @@Description      :  html document
+# @@File             :  index.php
+# @@Description      :  php document
 # @@Changelog        :  Updated header
 # @@TODO             :  
 # @@Other            :  
@@ -42,22 +42,63 @@
   <link rel="icon" type="image/icon png" href="./images/icon.png" />
   <link rel="apple-touch-icon" href="./images/icon.png" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+  <link rel="stylesheet" type="text/css" href="./css/cookieconsent.css" />
+  <link rel="stylesheet" href="./css/bootstrap.css" />
+  <link rel="stylesheet" href="./css/index.css" />
   <script src="./js/errorpages/isup.js"></script>
   <script src="./js/errorpages/homepage.js"></script>
   <script src="./js/errorpages/loaddomain.js"></script>
   <script src="./js/jquery/default.js"></script>
   <script src="./js/passprotect.min.js" defer></script>
+  <script src="./js/bootstrap.min.js" defer></script>
   <script src="./js/app.js" defer></script>
 </head>
 
-<body>
-  <div class="container" style="text-align: center; align-items: center">
+<body class="container text-center" style="align-items: center; justify-content: center">
+  <h1 class="m-5">Congratulations</h1>
+  <h2>
+    Your REPLACE_SERVER_SOFTWARE container has been setup.<br />
+    This file is located in:
+    <?php echo $_SERVER['DOCUMENT_ROOT']; ?>
     <br /><br />
-    <h1 class="m-5">Congratulations PHP has been successfully configured</h1>
-    <br /><br />
-    <?php phpinfo(); ?>
-    <br /><br />
+
+    SERVER:
+    <?php echo $_SERVER['SERVER_SOFTWARE']; ?> <br />
+    SERVER Address:
+    <?php echo $_SERVER['SERVER_ADDR']; ?> <br />
+
+  </h2>
+  <br /><br />
+  <br /><br />
+
+  <br /><br />
+  <!-- Begin EU compliant -->
+  <div class="text-center align-items-center fs-3">
+    <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+    <script>
+      window.cookieconsent.initialise({
+        palette: {
+          popup: {
+            background: '#64386b',
+            text: '#ffcdfd',
+          },
+          button: {
+            background: '#f8a8ff',
+            text: '#3f0045',
+          },
+        },
+        theme: 'edgeless',
+        content: {
+          message:
+            'This site uses cookie and in accordance with the EU GDPR<br />law this message is being displayed.<br />',
+          dismiss: 'Dismiss',
+          link: 'CasjaysDev Privacy Policy',
+          href: 'https://casjaysdev.com/policy',
+        },
+      });
+    </script>
   </div>
+  <!-- End EU compliant -->
 </body>
 
 </html>
